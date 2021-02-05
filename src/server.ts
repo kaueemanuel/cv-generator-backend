9bb5e046f66bbb77c40f06bde6d870e66daa2eb4
Kauê Brasil <kaue.malheiros@gmail.com>
2021-02-05 02:48:32 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,25 @@ import routes from './routes';
 import cors from 'cors';
 
 const app = express();
-
+const originAux = ['*'];
 const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: (origin, callback) => {
+    if (
+      !origin ||
+      originAux.indexOf('*') !== -1 ||
+      originAux.indexOf(origin) !== -1
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 200, //some legacy browsers (IE11, various SmartTVs) choke on 204
 };
+
+app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(routes);
