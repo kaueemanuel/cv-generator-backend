edf0ef1f829390a6b691ddb6c37191f1970fce79
Kauê Brasil <kaue.malheiros@gmail.com>
2021-02-05 03:08:42 -0300

    env

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,21 @@
 import express from 'express';
 import routes from './routes';
 import * as cors from 'cors';
+import dotenv from 'dotenv';
 
+dotenv.config({
+  path: './env/.env',
+});
 const app = express();
-const originAux = ['*'];
-const corsOptions = {
-  origin: (origin: any, callback: any) => {
-    if (
-      !origin ||
-      originAux.indexOf('*') !== -1 ||
-      originAux.indexOf(origin) !== -1
-    ) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+const corsOptions: cors.CorsOptions = {
+  origin: '*',
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
   optionsSuccessStatus: 200, //some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
