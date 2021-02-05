30b5a112d24206cabaed6149a36951577ee2dc24
Kauê Brasil <kaue.malheiros@gmail.com>
2021-02-05 02:36:42 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
