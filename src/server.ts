1851d3538f6b35dc683a399d331ca68bc019c250
Kauê Brasil <kaue.malheiros@gmail.com>
2021-02-05 02:49:51 -0300

    adsf

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
