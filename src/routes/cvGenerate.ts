36f085b59f31632d611dbfbe769f34cbf81c45ae
Kauê Brasil <kaue.malheiros@gmail.com>
2021-02-05 05:18:49 -0300

    pdf generator

--- src/routes/cvGenerate.ts
+++ src/routes/cvGenerate.ts
@@ -1,8 +1,54 @@
 import { Router } from 'express';
+import pdf from 'dynamic-html-pdf';
+import fs from 'fs';
+import path from 'path';
 
 const routes = Router();
 
 routes.get('/', (request, response) => {
+  const html = fs.readFileSync(path.resolve('src', 'templates', 'cv.html'), {
+    encoding: 'utf-8',
+  });
+
+  // Custom handlebar helper
+  pdf.registerHelper('ifCond', function (v1, v2, options) {
+    if (v1 === v2) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
+  });
+  const options = {
+    format: 'A4',
+    orientation: 'portrait',
+    border: '10mm',
+  };
+
+  const user = {
+    name: 'Kauê',
+  };
+
+  const document = {
+    type: 'buffer', // 'file' or 'buffer'
+    template: html,
+    context: {
+      avatar: path.resolve('src', 'uploads', 'avatar.jpg'),
+      user,
+    },
+    path: './output.pdf', // it is not required if type is buffer
+  };
+
+  pdf
+    .create(document, options)
+    .then(res => {
+      console.log(res);
+      fs.writeFileSync(path.resolve('src', 'tmp', 'teste.pdf'), res, {
+        encoding: 'utf-8',
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
   response.json({ message: 'generate cv!' });
 });
 routes.post('/', (request, response) => {
